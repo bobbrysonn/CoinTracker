ebb33d1dcc3b074f45217e4cdfaed164a5bed9c5
Bob Moriasi <107801004+bobbrysonn@users.noreply.github.com>
2024-04-23 01:39:34 -0400

    Initial Commit

---
+++ CoinTracker/CoinTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTrackerApp.swift
+//  CoinTracker
+//
+//  Created by Bob Moriasi on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
